e78d4af129a2baf6e68d5774e84dfc9d06586315
oopanpan <oopanpan@gmail.com>
2021-02-09 22:19:23 -0800

    commit tree 136f2cfae144d99ccffd57de7db78ade4b7d182c

--- app/controllers/blogger_controller.rb
+++ app/controllers/blogger_controller.rb
@@ -1,2 +1,30 @@
 class BloggerController < ApplicationController
+    before_action :find_blogger, only: [:show, :edit, :update, :destoy]
+
+    def index
+        @bloggers = Blogger.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+
+
+
+
+    private
+
+    def find_blogger
+        @blogger = Blogger.find(params[:id])
+    end
+
+    def blogger_params
+        params.require(:blogger).permit(:name, :bio, :age)
+    end
 end
